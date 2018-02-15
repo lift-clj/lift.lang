a124ea961b903c4773c041a01696be92976e46de
Andrew Mcveigh <me@andrewmcveigh.com>
2018-02-15 18:13:49 +0000

    Add x binding to decision tree

--- dev/dev.clj
+++ dev/dev.clj
@@ -29,40 +29,28 @@
 (defn var? [x]
   (and (simple-symbol? x) (re-matches #"^[^A-Z].*$" (name x))))
 
-(defn ing-bound? [tree]
-  (or (some var? (keys (:bind tree))) (:dflt tree)))
-
-(defn ing-binding [{:keys [bind dflt]}]
-  (or dflt (->> bind (filter (comp var? key)) (into {}))))
-
-(defn fully-bound? [{:keys [type] :as x}]
-  (c/case type
-    :bind (every? fully-bound? (:ings x))
-    :ing  (and (ing-bound? x)
-               (every? (comp fully-bound? val) (:dest x)))))
-
 (let [n (atom 0)]
   (defn gsym [& _]
     (symbol (str "_" (swap! n inc)))))
 
 (declare case-tree)
 
-(defn bind [n m e l]
+(defn bind [n x m e l]
   (if n
     (if l
       (-> (update n :bind conj m) (update :then l))
       (throw (Exception. "Trying to make final binding twice")))
     (if l
-      {:type :bind :bind [m] :then (l nil)}
-      {:type :bind :bind [m] :expr e})))
+      {:type :bind :bind [m] :thex x :then (l nil)}
+      {:type :bind :bind [m] :thex x :expr e})))
 
-(defn dest [n m e l]
+(defn dest [n x m e l]
   (let [[p & ms] m
         prjs (map (juxt gsym identity) ms)
         sjrp (reverse prjs)
         l-fn (fn [l [gs m]]
                (fn [n]
-                 (case-tree n m e l)))
+                 (case-tree n gs m e l)))
         l'   (reduce l-fn (l-fn l (first sjrp)) (rest sjrp))]
     (if n
       (if (= p (:test n))
@@ -70,32 +58,33 @@
         (update n :else l' ))
       {:type :dest
        :test p
+       :thex x
        :prjs (mapv first prjs)
        :then (l' nil)})))
 
-(defn lit-eq [n m e l]
+(defn lit-eq [n x m e l]
   (if n
     (if (= m (:ltrl n))
       (update n :then l)
       (update n :else l))
     (if l
-      {:type :ltrl :ltrl m :then (l nil)}
-      {:type :ltrl :ltrl m :expr e})))
+      {:type :ltrl :ltrl m :thex x :then (l nil)}
+      {:type :ltrl :ltrl m :thex x :expr e})))
 
-(defn case-tree [n m e l]
+(defn case-tree [n x m e l]
   (let [t (:type n)]
     (cond (and (vector? m) (or (nil? n) (= t :dest)))
-          (dest n m e l)
+          (dest n x m e l)
           (vector? m)
           (throw (Exception. "Trying to match pattern after irrefutable bind"))
           (and (var? m) (or (nil? n) (= t :bind)))
-          (bind n m e l)
+          (bind n x m e l)
           (var? m)
-          (update n :else case-tree m e l)
+          (update n :else case-tree x m e l)
           (or (nil? n) (= t :ltrl))
-          (lit-eq n m e l)
+          (lit-eq n x m e l)
           :else
-          (update n :else case-tree m e l))))
+          (update n :else case-tree x m e l))))
 
 (defn default-case [{:keys [type then else] :as n} default]
   (if (contains? #{:dest :ltrl} type)
@@ -108,48 +97,66 @@
         n))
     n))
 
-(-> nil
-    (case-tree '[Pair [Pair [Just a] [Just b]] c] '[a b c] nil)
-    (case-tree '[Pair [Pair       a  [Just b]] c] '[a b c] nil)
-    (case-tree '[Pair [Pair       a        b ] c] '[a b c] nil)
-    ;; (case-tree '[Pair             a        b    ] '[a b  ] nil)
-    (case-tree 'Nothing                           '[     ] nil)
-    (case-tree '_                                 '[     ] nil)
-    (default-case nil))
+(def case-tree-1
+  (let [x 'Nothing]
+    (-> nil
+        (case-tree x '[Pair [Pair [Just a] [Just b]] c] '[a b c] nil)
+        (case-tree x '[Pair [Pair       a  [Just b]] c] '[a b c] nil)
+        (case-tree x '[Pair [Pair       a        b ] c] '[a b c] nil)
+        (case-tree x '[Pair             a        b    ] '[a b  ] nil)
+        (case-tree x 'Nothing                           '[     ] nil)
+        (case-tree x '_                                 '[     ] nil)
+        (default-case nil))))
+
+(case x
+  [Pair a b] [a b]
+  )
 
 {:type :dest,
  :test Pair,
- :prjs [_218 _219],
+ :thex Nothing,
+ :prjs [_263 _264],
  :then {:type :dest,
         :test Pair,
-        :prjs [_220 _221],
+        :thex _263,
+        :prjs [_265 _266],
         :then {:type :dest,
                :test Just,
-               :prjs [_222],
+               :thex _265,
+               :prjs [_267],
                :then {:type :bind,
                       :bind [a],
+                      :thex _267,
                       :then {:type :dest,
                              :test Just,
-                             :prjs [_223],
+                             :thex _266,
+                             :prjs [_268],
                              :then {:type :bind,
                                     :bind [b],
-                                    :then {:type :bind, :bind [c], :expr [a b c]}}}},
+                                    :thex _268,
+                                    :then {:type :bind, :bind [c], :thex _264, :expr [a b c]}}}},
                :else {:type :bind,
                       :bind [a a],
+                      :thex _271,
                       :then {:type :dest,
                              :test Just,
-                             :prjs [_228],
+                             :thex _272,
+                             :prjs [_273],
                              :then {:type :bind,
                                     :bind [b],
-                                    :then {:type :bind, :bind [c], :expr [a b c]}},
+                                    :thex _273,
+                                    :then {:type :bind, :bind [c], :thex _270, :expr [a b c]}},
                              :else {:type :bind,
                                     :bind [b],
-                                    :then {:type :bind, :bind [c], :expr [a b c]}}}}},
+                                    :thex _277,
+                                    :then {:type :bind, :bind [c], :thex _275, :expr [a b c]}}}}},
         :else {:type :ltrl,
                :ltrl Nothing,
+               :thex Nothing,
                :expr [],
-               :else {:type :bind, :bind [_], :expr []}}},
+               :else {:type :bind, :bind [_], :thex Nothing, :expr []}}},
  :else {:type :ltrl,
         :ltrl Nothing,
+        :thex Nothing,
         :expr [],
-        :else {:type :bind, :bind [_], :expr []}}}
+        :else {:type :bind, :bind [_], :thex Nothing, :expr []}}}
